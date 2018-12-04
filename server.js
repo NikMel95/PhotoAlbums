381484b2479f31daf8f28220c72b9cf6da875c37
4ukuta <nikita.melnikov95@mail.com>
2018-12-04 19:16:26 +0300

    Change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 5000;
+const port = 8080;
 const clientDir = '';
 
 app.use(bodyParser.json());
